8bbed95f1aeb539845fc03573b298d511d057dc6
Abel Salgado Romero <romeroab@vmware.com>
2024-04-11 11:24:17 +0200

    Initial commit: working reproducer + basic README

---
+++ src/main/java/org/scg/cors/CorsIssueApplication.java
@@ -0,0 +1,13 @@
+package org.scg.cors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorsIssueApplication.class, args);
+	}
+
+}
